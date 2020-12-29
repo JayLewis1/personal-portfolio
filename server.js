d5606b999e2b9bac9923a80f01c180b25b9d7c34
Jay Lewis <jr_lewis@outlook.com>
2020-12-29 11:41:32 +0000

    Updates to projects button positions

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const path = require("path");
 const cors = require("cors");
 const { Console } = require("console");
 
+const favicon = require('express-favicon');
+
 const app = express();
 
 const PORT = process.env.PORT || 5000;
@@ -20,6 +22,9 @@ app.use(bodyParser.json())
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.json({ extended: false }));
+
+// Use Favicon Icon
+app.use(favicon(__dirname + '/build/favicon.ico'));
 
 
 // app.get("/", (req, res) => res.send("API running"));
